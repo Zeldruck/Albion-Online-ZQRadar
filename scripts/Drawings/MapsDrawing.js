6bd40eda005e356c12503dee5812d0c4a5ed4c1f
Zeldruck <felix.jouannneau@gmail.com>
2024-01-02 01:35:35 -0500

    Fix map background && delete logs

--- scripts/Drawings/MapsDrawing.js
+++ scripts/Drawings/MapsDrawing.js
@@ -33,50 +33,21 @@ export class MapDrawing extends DrawingUtils
         ctx.fillStyle = '#1a1c23';
         ctx.fillRect(0, 0, ctx.width, ctx.height);
 
-        if (!this.settings.settingShowMap) return;
+        if (!this.settings.showMapBackground) return;
 
         const src = "/images/Maps/" + drawTo + ".png";
 
         const preloadedImage = this.settings.GetPreloadedImage(src, "maps");
 
-
         if (preloadedImage)
         {
             ctx.save();
-
-            /*
-
-            const angle = -0.7071;
-
-            let newX = x * angle - y * angle;
-            let newY = x * angle + y * angle;
-            newX *= 4;
-            newY *= 4;
-
-            newX += 250;
-            newY += 250;
-
-            return { x: newX, y: newY };
-
-            */
-
-            /*ctx.translate(250, 250);
-            //ctx.translate(-newX, -newY);
-            //ctx.rotate(0.7071);
-            ctx.rotate(0.785398);
-            ctx.translate(-x, -y);
-            ctx.scale(1, -1);
-            
-             
-            ctx.drawImage(preloadedImage, -size/2, -size/2, size, size);
-            ctx.restore();*/
 
             ctx.scale(1, -1);
             ctx.translate(250, -250);
             
             ctx.rotate(-0.785398);
             ctx.translate(-x, y);
-            //ctx.translate(-x, -y);
              
             ctx.drawImage(preloadedImage, -size/2, -size/2, size, size);
             ctx.restore();
