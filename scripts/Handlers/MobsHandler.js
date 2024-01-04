406e8b5fc0ae73117ea7b919cfac96ea4264770b
Zeldruck <felix.jouannneau@gmail.com>
2024-01-04 14:58:35 -0500

    Separating enemies & living resources & enchants & tiers

--- scripts/Handlers/MobsHandler.js
+++ scripts/Handlers/MobsHandler.js
@@ -1,5 +1,16 @@
-class Mob {
-    constructor(id, typeId, posX, posY, health, enchantmentLevel, rarity) {
+const EnemyType = 
+{
+    LivingHarvestable: 0,
+    LivingSkinnable: 1,
+    Enemy: 2, // ??? not sure
+    Boss: 3, // ??? not sure either
+    Drone: 4,
+};
+
+class Mob
+{
+    constructor(id, typeId, posX, posY, health, enchantmentLevel, rarity)
+    {
         this.id = id;
         this.typeId = typeId;
         this.posX = posX;
@@ -8,17 +19,19 @@ class Mob {
         this.enchantmentLevel = enchantmentLevel;
         this.rarity = rarity;
         this.tier = 0;
-        this.type = 2;
+        this.type = EnemyType.Enemy;
         this.name = null;
         this.exp = 0;
         this.hX = 0;
         this.hY = 0;
-
     }
 }
 
-class Mist {
-    constructor(id, posX, posY, name, enchant) {
+// MIST PORTALS ??
+class Mist
+{
+    constructor(id, posX, posY, name, enchant)
+    {
         this.id = id;
         this.posX = posX;
         this.posY = posY;
@@ -27,195 +40,324 @@ class Mist {
         this.hX = 0;
         this.hY = 0;
 
-        if (name.toLowerCase().includes("solo")) {
+        if (name.toLowerCase().includes("solo"))
+        {
             this.type = 0;
-        } else {
+        }
+        else
+        {
             this.type = 1;
         }
     }
 }
 
+class MobsHandler
+{
+    constructor(settings)
+    {
+        this.settings = settings;
 
-
-
-
-
-
-
-
-class MobsHandler {
-    constructor() {
         this.mobsList = [];
         this.mistList = [];
         this.mobinfo = {};
 
+        this.harvestablesNotGood = [];
+
+        const logEnemiesList = document.getElementById("logEnemiesList");
+        if (logEnemiesList)
+            logEnemiesList.addEventListener("click", () => console.log(this.mobsList));
     }
-    updateMobInfo(newData) {
+
+    updateMobInfo(newData)
+    {
         this.mobinfo = newData;
     }
 
-    clear() {
-
-
+    clear()
+    {
         this.mobsList = [];
         this.mistList = [];
-      
     }
 
-    NewMobEvent(parameters) {
+    NewMobEvent(parameters)
+    {
         const id = parseInt(parameters[0]); // entity id
         let typeId = parseInt(parameters[1]); // real type id
-
 
         const loc = parameters[7];
         let posX = loc[0];
         let posY = loc[1];
 
         let exp = 0
-
-        try {
+        try
+        {
             exp = parseFloat(parameters[13]);
-        } catch (error) {
+        }
+        catch (error)
+        {
             exp = 0;
         }
 
         let name = null;
-        try {
+        try
+        {
             name = parameters[32];
-        } catch (error) {
-            name = null;
         }
-
-        let enchant = null;
-        try {
-            enchant = parameters[33];
-        } catch (error) {
-            enchant = null;
-        }
-
-        if (name == null) {
-            try {
+        catch (error)
+        {
+            try
+            {
                 name = parameters[31];
-            } catch (error) {
+            }
+            catch (error2)
+            {
                 name = null;
             }
         }
 
-        let rarity = 1;
+        let enchant = 0;
+        try
+        {
+            enchant = parameters[33];
+        }
+        catch (error)
+        {
+            enchant = 0;
+        }
 
-        try {
+        let rarity = 1;
+        try
+        {
             rarity = parseInt(parameters[19]);
-        } catch (error) {
+        }
+        catch (error)
+        {
             rarity = 1;
         }
 
-        if (name != null )
+        if (name != null)
         {
-            if (this.mistList.some(mist => mist.id === id))
-                return;
-
-            this.addMist(id, posX, posY, name, enchant);
+            this.AddMist(id, posX, posY, name, enchant);
         }
         else
         {
-            if (this.mobsList.some(mob => mob.id === id))
-                return;
-
-            this.addMob(id, typeId, posX, posY, exp, 0, rarity);
+            this.AddEnemy(id, typeId, posX, posY, exp, enchant, rarity);
         }
-    
-
     }
     
 
-    addMob(id, typeId, posX, posY, health, enchant, rarity)
+    AddEnemy(id, typeId, posX, posY, health, enchant, rarity)
     {
+        if (this.mobsList.some(mob => mob.id === id))
+            return;
+
+        if (this.harvestablesNotGood.some(mob => mob.id === id))
+            return;
+
         const h = new Mob(id, typeId, posX, posY, health, enchant, rarity);
 
-        if (this.mobinfo[typeId]!=null) {
+        // TODO
+        // List of enemies
+        if (this.mobinfo[typeId] != null) 
+        {
             const mobsInfo = this.mobinfo[typeId];
-            
+
             h.tier = mobsInfo[0];
             h.type = mobsInfo[1];
             h.name = mobsInfo[2];
+
+            // TODO
+            // better than this
+            if (h.type == EnemyType.LivingSkinnable)
+            {
+                /* If animal is enchanted, it'll probably never work and jump into this return
+                   Because it's sending an event with normal animal with tier, ect
+                   And after send another event to say, this animal is enchant Y
+                   And it's the same with the other living harvestables
+                   But keep that in case it changes
+                */
+                if ((!this.settings.harvestingLivingHideTiers[h.tier-1] || !this.settings.harvestingLivingHideEnchants[enchant]))
+                {
+                    this.harvestablesNotGood.push(h);
+                    return;
+                }
+            }
+            else if (h.type == EnemyType.LivingHarvestable)
+            {
+                if (h.name == "ore")
+                {
+                    if ((!this.settings.harvestingLivingOreTiers[h.tier-1] || !this.settings.harvestingLivingOreEnchants[enchant]))
+                    {
+                        this.harvestablesNotGood.push(h);
+                        return;
+                    }
+                }
+                else if (h.name == "rock")
+                {
+                    if ((!this.settings.harvestingLivingRockTiers[h.tier-1] || !this.settings.harvestingLivingRockEnchants[enchant]))
+                    {
+                        this.harvestablesNotGood.push(h);
+                        return;
+                    }
+                }
+                else if (h.name == "wood")
+                {
+                    if ((!this.settings.harvestingLivingLogTiers[h.tier-1] || !this.settings.harvestingLivingLogEnchants[enchant]))
+                    {
+                        this.harvestablesNotGood.push(h);
+                        return;
+                    }
+                }
+                else if (h.name == "fiber")
+                {
+                    if ((!this.settings.harvestingLivingFiberTiers[h.tier-1] || !this.settings.harvestingLivingFiberEnchants[enchant]))
+                    {
+                        this.harvestablesNotGood.push(h);
+                        return;
+                    }
+                }
+            }
         }
 
         this.mobsList.push(h);
     }
 
-    addMist(id, posX, posY, name, enchant)
+    removeMob(id)
     {
+        let pSize = this.mobsList.length;
+
+        this.mobsList = this.mobsList.filter((x) => x.id !== id);
+
+        // That means we already removed the enemy, so it can't be in the other list
+        if (this.mobsList < pSize) return;
+
+        this.harvestablesNotGood = this.harvestablesNotGood.filter((x) => x.id !== id);
+    }
+
+    updateMobPosition(id, posX, posY)
+    {
+        var enemy = this.mobsList.find((enemy) => enemy.id === id);
+
+        if (enemy)
+        {
+            enemy.posX = posX;
+            enemy.posY = posY;
+
+            return;
+        }
+
+        enemy = this.harvestablesNotGood.find((enemy) => enemy.id === id);
+
+        if (!enemy) return;
+
+        enemy.posX = posX;
+        enemy.posY = posY;
+    }
+
+    updateEnchantEvent(parameters)
+    {
+        const mobId = parameters[0];
+        const enchantmentLevel = parameters[1];
+
+        var enemy = this.mobsList.find((mob) => mob.id == mobId);
+
+        if (enemy)
+        {
+            enemy.enchantmentLevel = enchantmentLevel;
+            return;
+        }
+
+        // Else try in our not good list
+        enemy = this.harvestablesNotGood.find((mob) => mob.id == mobId);
+
+        if (!enemy) return;
+
+        enemy.enchantmentLevel = enchantmentLevel;
+
+        let hasToSwapFromList = false;
+
+        if (enemy.type == EnemyType.LivingSkinnable)
+        {
+            if (!this.settings.harvestingLivingHideTiers[enemy.tier-1] && !this.settings.harvestingLivingHideEnchants[enemy.enchantmentLevel])
+                return;
+
+            hasToSwapFromList = true;
+        }
+        else if (enemy.type == EnemyType.LivingHarvestable)
+        {
+            if (enemy.name == "ore")
+            {
+                if (!this.settings.harvestingLivingOreTiers[enemy.tier-1] || !this.settings.harvestingLivingOreEnchants[enemy.enchantmentLevel])
+                    return;
+
+                hasToSwapFromList = true;
+            }
+            else if (enemy.name == "rock")
+            {
+                if (!this.settings.harvestingLivingRockTiers[enemy.tier-1] || !this.settings.harvestingLivingRockEnchants[enemy.enchantmentLevel])
+                    return;
+
+                 hasToSwapFromList = true;
+            }
+            else if (enemy.name == "wood")
+            {
+                if (!this.settings.harvestingLivingLogTiers[enemy.tier-1] || !this.settings.harvestingLivingLogEnchants[enemy.enchantmentLevel])
+                    return;
+
+                hasToSwapFromList = true;
+            }
+            else if (enemy.name == "fiber")
+            {
+                if (!this.settings.harvestingLivingFiberTiers[enemy.tier-1] || !this.settings.harvestingLivingFiberEnchants[enemy.enchantmentLevel])
+                    return;
+
+                hasToSwapFromList = true;
+            }
+        }
+
+        if (!hasToSwapFromList) return;
+
+        this.mobsList.push(enemy);
+        this.harvestablesNotGood = this.harvestablesNotGood.filter((x) => x.id !== enemy.id);
+    }
+
+    getMobList()
+    {
+        return [...this.mobsList];
+    }
+
+
+    AddMist(id, posX, posY, name, enchant)
+    {
+        if (this.mistList.some((mist) => mist.id === id))
+            return;
+
         const d = new Mist(id, posX, posY, name, enchant);
 
         this.mistList.push(d);
     }
 
-    removeMist(id) {
- 
-        this.mistList = this.mistList.filter(mist => mist.id !== id);
-
+    removeMist(id)
+    {
+        this.mistList = this.mistList.filter((mist) => mist.id !== id);
     }
 
+    updateMistPosition(id, posX, posY)
+    {
+        var mist = this.mistList.find((mist) => mist.id === id);
 
-    updateMistPosition(id, posX, posY) {
-        this.mistList.forEach(mist => {
-            if (mist.id === id) {
-                mist.posX = posX;
-                mist.posY = posY;
-            }
-        });
+        if (!mist) return;
+
+        mist.posX = posX;
+        mist.posY = posY;
     }
 
-    updateMistEnchantmentLevel(mistId, enchantmentLevel) {
-        const mistToUpdate = this.mistList.find(mist => mist.id === mistId);
-        if (mistToUpdate) {
-            mistToUpdate.enchant = enchantmentLevel;
-        }
-    }
+    updateMistEnchantmentLevel(id, enchantmentLevel)
+    {
+        var mist = this.mistList.find((mist) => mist.id === id);
 
+        if (!mist) return;
 
-    updateMobEnchantmentLevel(mobId, enchantmentLevel) {
-
-
-        for (const mob of this.mobsList) {
-            if (mob.id === mobId) {
-                mob.enchantmentLevel = enchantmentLevel;
-
-                break;
-            }
-        }
-
-    }
-    updateEnchantEvent(parameters) {
-    const mobId = parameters[0];
-    const enchantmentLevel = parameters[1];
-    this.updateMobEnchantmentLevel(mobId, enchantmentLevel);
-    }
-
-
-    removeMob(id) {
-
-            this.mobsList = this.mobsList.filter((x) => x.id !== id);
-     
-        
-    }
-
-    getMobList() {
-       
-         return [...this.mobsList];
-    
-    }
-
-    updateMobPosition(id, posX, posY) {
-   
-            for (const mob of this.mobsList) {
-                if (mob.id === id) {
-                    mob.posX = posX;
-                    mob.posY = posY;
-                    break;
-                }
-            }
-      
-    }
-
+        mist.enchant = enchantmentLevel;
+    }  
 }
