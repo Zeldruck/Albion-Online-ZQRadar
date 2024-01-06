66bf2a2b135277da0525fd0ffbd9ef75a2cc6027
Zeldruck <felix.jouannneau@gmail.com>
2024-01-06 00:50:15 -0500

    Changes on how mobs are drawn

--- scripts/Drawings/MobsDrawing.js
+++ scripts/Drawings/MobsDrawing.js
@@ -43,109 +43,63 @@ export class MobsDrawing extends DrawingUtils
     {
         for (const mobOne of mobs)
         {
-            if (mobOne.type == EnemyType.Boss && !this.settings.bossEnemies)
-            {
-                continue;
-            }
-            else if (mobOne.type == EnemyType.Drone && !this.settings.doneEnemies) 
-            {
-                continue;
-            } 
-            else
-            {
-                // TODO
-                // Check tier and enchant for each type
-                /*if (mobOne.type == 0 || mobOne.type == 1 && !this.settings.mobTiers[mobOne.tier - 1])
-                {
-                    continue;
-                }
-
-                if (!this.settings.mobEnchants[mobOne.enchantmentLevel])
-                {
-                    continue; 
-                }*/
-            }
-            
-
             const point = this.transformPoint(mobOne.hX, mobOne.hY);
 
-            if (mobOne.type == EnemyType.LivingSkinnable)
-            {
-                // Change folder to animals
-                this.DrawCustomImage(ctx, point.x, point.y, "hide_" + mobOne.tier + "_" + mobOne.enchantmentLevel, "Resources", 40);
-            }
-            else if (mobOne.type == EnemyType.LivingHarvestable)
-            {
-                // Change folder to animals
-                if (mobOne.name.includes("wood"))
-                {
-                    this.DrawCustomImage(ctx, point.x, point.y, "Logs_" + mobOne.tier + "_" + mobOne.enchantmentLevel, "Resources", 40);
-                }
-                else
-                {
-                    this.DrawCustomImage(ctx, point.x, point.y, mobOne.name + "_" + mobOne.tier + "_" + mobOne.enchantmentLevel, "Resources", 40);
-                }
-            }
-            else if (mobOne.type == EnemyType.Boss)
-            {
-                // Change folder to enemies
-                this.DrawCustomImage(ctx, point.x, point.y, this.name, "Resources", 40);
+            let imageName = undefined;
+            let imageFolder = undefined;
 
-            }
-            else if (mobOne.type == EnemyType.Drone)
-            {
-                // Change folder to enemies
-                this.DrawCustomImage(ctx, point.x, point.y, "droneicon", "Resources", 40);
-            }
-            else
-            {
-                // TODO
-                if (mobOne.enchantmentLevel == 1)
-                {
-                    this.drawFilledCircle(ctx, point.x, point.y, 15, "green");
-                }
-                else if (mobOne.enchantmentLevel == 2)
-                {
-                    this.drawFilledCircle(ctx, point.x, point.y, 15, "#45BDEE");
-                }
-                else if (mobOne.enchantmentLevel == 3)
-                {
-                    this.drawFilledCircle(ctx, point.x, point.y, 15, "purple");
-                }
-                else if (mobOne.enchantmentLevel == 4)
-                {
-                    this.drawFilledCircle(ctx, point.x, point.y, 15, "yellow");
-                }
-                else
-                {
-                    this.drawFilledCircle(ctx, point.x, point.y, 10, "#4169E1");
-                }             
-            }
+            /* Set by default to enemy, since there are more, so we don't add at each case */
+            let drawHp = this.settings.enemiesHP;
+            let drawId = this.settings.enemiesID;
 
-            let drawHp = false;
-            let drawId = false;
-
-            if (mobOne.type == EnemyType.LivingHarvestable || mobOne.type == EnemyType.LivingSkinnable)
+            if (mobOne.type == EnemyType.LivingSkinnable || mobOne.type == EnemyType.LivingHarvestable)
             {
+                imageName = mobOne.name + "_" + mobOne.tier + "_" + mobOne.enchantmentLevel;
+                imageFolder = "Resources"; // Change folder to living harvestables
+
                 drawHp = this.settings.livingResourcesHp;
                 drawId = this.settings.livingResourcesID;
             }
-            else
+            else if (mobOne.type >= EnemyType.Enemy && mobOne.type <= EnemyType.Boss)
             {
+                imageName = mobOne.name;
+                imageFolder = "Resources"; // Change folder to enemies
+
+                drawHp = this.settings.enemiesHP;
+                drawId = this.settings.enemiesID;
+            }
+            else if (mobOne.type == EnemyType.Drone)
+            {
+                imageName = mobOne.name;
+                imageFolder = "Resources"; // Change folder to enemies
+
+                drawHp = this.settings.enemiesHP;
+                drawId = this.settings.enemiesID;
+            }
+            else if (mobOne.type == EnemyType.MistBoss)
+            {
+                imageName = mobOne.name;
+                imageFolder = "Resources"; // Change folder to enemies
+
                 drawHp = this.settings.enemiesHP;
                 drawId = this.settings.enemiesID;
             }
 
+            if (imageName !== undefined && imageFolder !== undefined)
+                this.DrawCustomImage(ctx, point.x, point.y, imageName, imageFolder, 40);
+            else
+                this.drawFilledCircle(ctx, point.x, point.y, 10, "#4169E1"); // Unmanaged ids
+
             if (drawHp)
             {
+                // TODO
+                // Draw health bar?
                 const textWidth = ctx.measureText(mobOne.health).width;
                 this.drawTextItems(point.x - textWidth /2, point.y + 24, mobOne.health, ctx, "12px", "yellow");
             }
 
             if (drawId)
-            {
                 this.drawText(point.x, point.y - 20, mobOne.typeId, ctx);
-            }
         }
 
         /* Mist portals */
